45e78100c93b48cd7482b751e3237cd13fb5f9f6
Reshoan Yazdi <reshoanyazdi@gmail.com>
2024-09-17 18:43:28 +0600

    test

--- missing_values_check.py
+++ missing_values_check.py
@@ -6,6 +6,7 @@ df = pd.read_csv('alzheimers_disease_data.csv')
 # Checking for missing values
 missing_values = df.isnull().sum()
 print("Missing Values in Each Column:\n", missing_values)
+#this is a test
 
 # Checking the percentage of missing values
 missing_percentage = (df.isnull().sum() / len(df)) * 100
